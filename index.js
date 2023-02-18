ccabe22af651ac6fcb2c2e5d0bacca3daf87e98e
Artur7798 <202108270938@alunos.estacio.br>
2023-02-17 21:55:51 -0300

    Ja consegui fazer a funcionalidade de adicionar funcionar, quero adicionar os nomes e depois jogar o placar final para uma tabela ou algo do tipo

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el") // pass in arguments
+let countEl2 = document.getElementById("count-el2") // pass in arguments
+let placar = "O placar final ficou: "
+
+
 let count = 0
-let count1 = document.getElementById("count1")
+let count2 = 0
 
 function increment() {
-    count = count + 1
-    count1.innerText = count
+    count += 1
+    countEl.textContent = count
+}
+function increment2() {
+    count2 += 1
+    countEl2.textContent = count2
+}
+function save(){
+    let countStr= count + " -"
+    let countStr2= count2
+    saveEl.textContent = placar + countStr +" " + countStr2
+    countEl.textContent = 0
+    count = 0
+    countEl2.textContent = 0
+    count2 = 0
 }

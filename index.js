7f79476b55b6660b30476bd880d5ccfc4cb4b41c
Artur7798 <202108270938@alunos.estacio.br>
2023-02-18 10:53:14 -0300

    Consegui trazer o nome das pessoas e do time, mas ainda quero salvar em uma tabela os resultados e ver se da para fazer um loop para o input. assim com um input consigo colocar o nome dos 2 times

--- index.js
+++ index.js
@@ -2,25 +2,37 @@ let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el") // pass in arguments
 let countEl2 = document.getElementById("count-el2") // pass in arguments
 let placar = "O placar final ficou: "
+let inputText = document.getElementById('response_name');
+let inputText2 = document.getElementById('response_time');
+let outputField = document.getElementById('total_response');
+let time1 = document.getElementById('time1');
 
 
 let count = 0
 let count2 = 0
 
-function increment() {
-    count += 1
-    countEl.textContent = count
-}
-function increment2() {
-    count2 += 1
-    countEl2.textContent = count2
-}
-function save(){
-    let countStr= count + " -"
-    let countStr2= count2
-    saveEl.textContent = placar + countStr +" " + countStr2
-    countEl.textContent = 0
-    count = 0
-    countEl2.textContent = 0
-    count2 = 0
-}
+    function increment() {
+        count += 1
+        countEl.textContent = count
+    }
+    function increment2() {
+        count2 += 1
+        countEl2.textContent = count2
+    }
+    function save(){
+        let countStr= count + " -"
+        let countStr2= count2
+        saveEl.textContent = placar + countStr +" " + countStr2
+        countEl.textContent = 0
+        count = 0
+        countEl2.textContent = 0
+        count2 = 0
+    }
+    function save_input(){
+            outputField.textContent = "Os participantes são: " + inputText.value + " " + "o nome do time é: " + inputText2.value
+            time1.textContent = inputText2.value
+        }
+
+
+
+

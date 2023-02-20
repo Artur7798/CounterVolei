e1cf964a95d45b659e4ad0181e5894225fc60149
Artur7798 <202108270938@alunos.estacio.br>
2023-02-20 16:52:15 -0300

    Tabela e looping adicionado

--- index.js
+++ index.js
@@ -6,16 +6,24 @@ let inputText = document.getElementById('response_name');
 let inputText2 = document.getElementById('response_time');
 let outputField = document.getElementById('total_response');
 let time1 = document.getElementById('time1');
-
+let time2 = document.getElementById('time2');
 
 let count = 0
 let count2 = 0
 
     function increment() {
-        count += 1
+    if (count>=22){
+        count-=1
+        countEl.textContent = count
+    } else
+    count += 1
         countEl.textContent = count
     }
     function increment2() {
+    if (count2>=22){
+        count2-=1
+        countEl2.textContent = count2
+    }  else
         count2 += 1
         countEl2.textContent = count2
     }
@@ -28,10 +36,20 @@ let count2 = 0
         countEl2.textContent = 0
         count2 = 0
     }
-    function save_input(){
-            outputField.textContent = "Os participantes são: " + inputText.value + " " + "o nome do time é: " + inputText2.value
-            time1.textContent = inputText2.value
-        }
+
+function saveInput() {
+    const inputText1 = document.getElementById("inputText1");
+    const inputText2 = document.getElementById("inputText2");
+    const tbody = document.getElementById("tbody");
+    const newRow = tbody.insertRow();
+    const participantCell = newRow.insertCell();
+    const teamCell = newRow.insertCell();
+    participantCell.textContent = inputText1.value;
+    teamCell.textContent = inputText2.value;
+    inputText1.value = "";
+    inputText2.value = "";
+}
+
 
 
 
